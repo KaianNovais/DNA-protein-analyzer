4c44b773febbe1bfd84f3aa7909fe0fc025f3b8a
Kaian Novais <55420281+KaianNovais@users.noreply.github.com>
2023-04-14 10:59:47 -0300

    Update DNA.java
    Formatação

--- DNA.java
+++ DNA.java
@@ -1,7 +1,7 @@
 public class DNA {
     public static void main(String[] args){
 
-        //define 3 dna qualquer, verdadeiro ou falso
+        //define 3 dna's quaisquer
         String dna1 = "ATGCGATACGCTTGA";
         String dna2 = "ATGCGATACGTGA";
         String dna3 = "ATTAATATGTACTGA";
@@ -10,24 +10,27 @@ public class DNA {
         String dnas[] = {dna1, dna2, dna3};
 
         
-        //varre o array e verifica se contém Proteína
+        //Varre o array e verifica se contém Proteína em cada DNA
        for(String dna : dnas ){
+           
          //Armazena o indíce de ATG e TGA
          int indexATG = dna.indexOf("ATG");
          int indexTGA = dna.indexOf("TGA");
  
  
-         //Verifica se o DNA é uma proteína ou não
+         //Verifica se o DNA contém uma proteína ou não
          if(indexATG >= 0 && indexTGA >= 0 && (indexATG - indexTGA) % 3 == 0 ){
-             //Armazena o DNA a uma String chamada Proteina
+           
            String protein = dna.substring(indexATG, indexTGA + 3);
            System.out.println("DNA: " + dna);
            System.out.println("Proteína: " + protein);
+             
          } else {
-             System.out.println("DNA: " + dna);
-           System.out.println("Não tem proteína");
+             
+            System.out.println("DNA: " + dna);
+            System.out.println("Não tem proteína");
+             
          }
        }
-        
-      }
+    }
 }
